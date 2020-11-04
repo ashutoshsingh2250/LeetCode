dd7653e6558e674d7592ba5e32071b5df4ea5f25
ashutoshsingh2250 <ashutoshsingh2250@gmail.com>
2020-11-04 11:40:10 +0530

    84 ms

---
+++ solutions/554. Brick Wall.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int leastBricks(vector<vector<int>>& a) {
+        
+        for(vector<int> &v:a) {
+            for(int i=1; i<v.size()-1; i++) {
+                v[i]+=v[i-1];
+            }
+        }
+        
+        //int maxi=0;
+        unordered_map<int,int> uom;
+        for(vector<int> &v:a) { 
+            for(int i=0; i<v.size()-1; i++) {
+                uom[v[i]]++;
+                //maxi=max(maxi, uom[v[i]]);
+            }
+        }
+        
+        int maxi=0;
+        for(auto &kvp: uom){
+            maxi=max(maxi, kvp.second);
+        }
+        
+        return a.size()-maxi;
+    }
+};
